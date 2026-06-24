94e749a2a08a7254f4d5bf5f2f766ff91ed70cfd
a1l1ke <onetenthplease@gmail.com>
2026-06-24 10:06:21 +0900

    BackendProgrammer 클래스 정의 및 main 메서드에 테스트 코드 추가

--- src/Solution01.java
+++ src/Solution01.java
@@ -1,7 +1,17 @@
 public class Solution01 {
     public static void main(String[] args) {
-
+        Programmer programmer = new Programmer();
+        programmer.name = "John";
+        programmer.language = "Java";
+        programmer.work();
+        BackendProgrammer bp = new BackendProgrammer();
+        bp.name = "Jane";
+        bp.language = "Python";
+        bp.work();
     }
+}
+
+class BackendProgrammer extends Programmer {
 }
 
 class Programmer {
