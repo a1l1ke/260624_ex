dd53cbd536f291423c834a288784518a257a1d8f
a1l1ke <onetenthplease@gmail.com>
2026-06-24 10:10:32 +0900

    Programmer 및 BackendProgrammer 생성자 추가 및 main 메서드 코드 수정

--- src/Solution01.java
+++ src/Solution01.java
@@ -1,22 +1,41 @@
 public class Solution01 {
     public static void main(String[] args) {
-        Programmer programmer = new Programmer();
-        programmer.name = "John";
-        programmer.language = "Java";
+//        Programmer programmer = new Programmer();
+        Programmer programmer = new Programmer("John", "Java");
+//        programmer.name = "John";
+//        programmer.language = "Java";
         programmer.work();
-        BackendProgrammer bp = new BackendProgrammer();
-        bp.name = "Jane";
-        bp.language = "Python";
+//        BackendProgrammer bp = new BackendProgrammer();
+        BackendProgrammer bp = new BackendProgrammer("Jane", "Python");
+//        bp.name = "Jane";
+//        bp.language = "Python";
         bp.work();
     }
 }
 
 class BackendProgrammer extends Programmer {
+    //    BackendProgrammer() {
+//        super();
+//        super(name, lauguage);
+//    }
+    BackendProgrammer(String name, String language) {
+        super(name, language); // this 생성자 체이닝처럼.
+    }
 }
 
 class Programmer {
     String name;
     String language;
+
+    // 직접 생성자를 만들어주었기 때문에 기본 생성자 (컴파일러가 만들어주는) 가 없어짐
+//    Programmer() {
+//        super();
+//    }
+
+    Programmer(String name, String language) {
+        this.name = name;
+        this.language = language;
+    }
 
     void work() {
         System.out.println(name + "은 " + language + "로 프로그래밍을 합니다.");
