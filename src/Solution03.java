3367898a63f7b3ed755a81d24d6ea7ed3430cf26
a1l1ke <onetenthplease@gmail.com>
2026-06-24 12:05:17 +0900

    Solution03 업캐스팅 및 다운캐스팅 예제 추가

--- src/Solution03.java
+++ src/Solution03.java
@@ -1,2 +1,17 @@
+// 업캐스팅, 다운캐스팅
 public class Solution03 {
+
+}
+
+class Animal {
+    void eat() {
+        System.out.println("Animal.eat"); // soutm
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void eat() {
+        System.out.println("Dog.eat");
+    }
 }
