0c90bc80a5e81ac3cf65d7fdae88222127fd5dd6
a1l1ke <onetenthplease@gmail.com>
2026-06-24 10:44:57 +0900

    Solution02 중첩 클래스 구조 변경 및 protected 제어자 테스트를 위한 HouseEngineer 추가

--- src/Solution02.java
+++ src/Solution02.java
@@ -2,27 +2,29 @@ public class Solution02 {
     public static void main(String[] args) {
         Engineer e1 = new Engineer();
         e1.work();
-        CarEngineer e2 = new CarEngineer();
+        CarEngineer e2 = new CarEngineer(); // super -> work -> getVersion <= 정적 바인딩
         e2.work();
         Engineer e3 = new CarEngineer();
         e3.work();
     }
-}
 
-class Engineer {
-    void work() {
-        System.out.println("this.getVersion() = " + this.getVersion());
-    }
+    public static class Engineer {
+        protected String value = "SECRET";
 
-    private String getVersion() {
-        return "1.0";
+        void work() {
+            System.out.println("this.getVersion() = " + this.getVersion());
+        }
+
+        private String getVersion() {
+            return "1.0";
+        }
     }
 }
 
 // 상속은 확장이다 (타입확장, 기능확장)
 // 상위/부모 클래스는 상대적으로 좁고, 덜 구현되었다 라는 의미다,
 // 하위/자식 클래스는 물려받을 것을 바탕으로 자신이 구현할 것 바꿀 것을 취사선택함
-class CarEngineer extends Engineer {
+class CarEngineer extends Solution02.Engineer {
     @Override
     public void work() { // 더 넓은 호출이 가능
 //    private void work() { // 더 좁은 호출은 불가능
