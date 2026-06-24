6dd5653f606f2a70f1c5d5b6b4bf02d7fa3de944
a1l1ke <onetenthplease@gmail.com>
2026-06-24 10:03:40 +0900

    Solution01에 main 메서드 추가 및 Programmer 클래스 정의

--- src/Solution01.java
+++ src/Solution01.java
@@ -1,2 +1,14 @@
 public class Solution01 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Programmer {
+    String name;
+    String language;
+
+    void work() {
+        System.out.println(name + "은 " + language + "로 프로그래밍을 합니다.");
+    }
 }
